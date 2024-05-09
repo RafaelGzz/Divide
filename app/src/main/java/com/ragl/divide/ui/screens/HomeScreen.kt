c50b97c87a9c09cb13022cdcf651762b6a86faef
RafaelGzz <ragl122778@gmail.com>
2024-05-09 05:39:10 -0600

    Added Home Screen with basic logout

--- app/src/main/java/com/ragl/divide/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/ragl/divide/ui/screens/HomeScreen.kt
@@ -1,9 +1,27 @@
 package com.ragl.divide.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun HomeScreen(){
-    Text(text = "Home Screen")
+fun HomeScreen(
+    vm: HomeViewModel = hiltViewModel(),
+    onLogOut: () -> Unit
+){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { vm.logout(onLogOut) }) {
+            Text(text = "Log Out")
+        }
+    }
 }
